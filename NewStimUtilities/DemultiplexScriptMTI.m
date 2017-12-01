fb1165709a93ce04f1faf253e56d97a084190bf7
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2017-12-01 11:48:28 -0500

    updated DemultiplexScriptMTI

--- NewStimUtilities/DemultiplexScriptMTI.m
+++ NewStimUtilities/DemultiplexScriptMTI.m
@@ -1,8 +1,8 @@
-function [stimscripttimestructlist] = DemultiplexScriptMTI(the_stimscripttimestruct, paramname);
+function [stimscripttimestructlist] = DemultiplexScriptMTI(the_stimscripttimestruct, paramname, includeblank);
 % DEMULTIPLEXSCRIPTMTI - Returns a script/MTI with a subset of specified stimuli
 %
 %   [STIMSCRIPTTIMESTRUCTLIST,PARAMVALS,...] =
-%      DEMULTIPLEXSCRIPTMTI(THE_STIMSCRIPTTIMESTRUCT, PARAMNAME)
+%      DEMULTIPLEXSCRIPTMTI(THE_STIMSCRIPTTIMESTRUCT, PARAMNAME, [INCLUDEBLANK])
 %
 %   "Demultiplexes" a STIMSCRIPTTIMESTRUCT (THE_STIMSCRIPTTIMESTRUCT) where many
 %   parameters are co-varied. PARAMNAME is the parameter that is demultiplexed;
@@ -10,7 +10,14 @@ function [stimscripttimestructlist] = DemultiplexScriptMTI(the_stimscripttimestr
 %   STIMSCRIPTTIMESTRUCT is created for each of the values of PARAMNAME that are
 %   encountered. These STIMSCRIPTTIMESTRUCTs are returned as 1xN list STIMSCRIPTTIMESTRUCTLIST.
 %
+%   If INCLUDEBLANK is included and is 1, then any blank stimulus is also
+%   included
+%
 %   See also: STIMSCRIPTTIMESTRUCT
+
+if nargin<3,
+    includeblank=0;
+end;
 
 paramvalues = [];
 paramstimlist = [];
@@ -20,6 +27,8 @@ mymti = the_stimscripttimestruct.mti;
 
  % step 1: find all the stims that have the parameter and have a value for that parameter
 
+blankid = []; 
+ 
 for i=1:numStims(myscript),
 	stim = get(myscript,i);
 	params = getparameters(stim);
@@ -27,6 +36,11 @@ for i=1:numStims(myscript),
 		paramvalues(end+1) = getfield(params,paramname);
 		paramstimlist(end+1) = i;
 	end;
+    if isfield(params,'isblank')
+        if params.isblank,
+            blankid = i;
+        end;
+    end;
 end;
 
 unique_values = unique(paramvalues);
@@ -37,6 +51,9 @@ stimscripttimestructlist = stimscripttimestructlist([]);
 for i=1:length(unique_values),
 	indvalues = find(paramvalues==unique_values(i));
 	stims_to_include = paramstimlist(indvalues);
+    if includeblank,
+        stims_to_include(end+1) = blankid;
+    end;
 	[newscript,dummy,newmti] = DecomposeScriptMTI(myscript,mymti,stims_to_include);
 	stimscripttimestructlist(end+1) = stimscripttimestruct(newscript,newmti);
 end;
