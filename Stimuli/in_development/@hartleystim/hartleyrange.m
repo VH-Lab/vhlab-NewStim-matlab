4d1af4302aaf361fd66aecf83180bb7521b41f0f
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2021-09-22 16:02:42 -0400

    almost done with graphical editing of Hartley stims

--- Stimuli/in_development/@hartleystim/hartleyrange.m
+++ Stimuli/in_development/@hartleystim/hartleyrange.m
@@ -8,7 +8,7 @@ function [s,kxv,kyv] = hartleyrange(HSstim)
 % The unconstrained range is every combination of -M:M for KXV and 
 % -M:M for KXY.  This is further limited by calculating the spatial frequency
 % of each stimulus, and rejecting any that have a spatial frequency less than
-% HSstim.HSparams.sfmmax.
+% HSstim.HSparams.sfmax.
 %
 
 Hp = getparameters(HSstim);
@@ -26,7 +26,7 @@ pixels_per_degree = pixels_per_cm * Hp.distance * tan(vlt.math.deg2rad(1));
 
 F_ = F*pixels_per_degree; % convert to cycles per degree of visual angle
 
-[I,J] = find(F_<=Hp.sfmmax & F_>0);
+[I,J] = find(F_<=Hp.sfmax & F_>0);
 
 kxv = kxv(J);
 kyv = kyv(I);
